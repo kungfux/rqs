c8128a23ae069288c904f1f9931194ca9074a2f8
kungfux <kungfux@755d531c-62fa-4a73-8645-5adfc4c11aca>
2013-05-23 13:34:27 +0000

    Working on text highlighting.
    git-svn-id: http://itworks.sytes.net/svn/isd-rqs/trunk@19 755d531c-62fa-4a73-8645-5adfc4c11aca

--- project/Logic/DataGridViewRichTextBoxCell.cs
+++ project/Logic/DataGridViewRichTextBoxCell.cs
@@ -1,39 +1,182 @@
-﻿/*   
- *  RQS
- *  Requirement Searching Utility
- *  Copyright (C) Fuks Alexander 2013
- *  
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *  
- *  Fuks Alexander, hereby disclaims all copyright
- *  interest in the program "RQS"
- *  (which makes passes at compilers)
- *  written by Alexander Fuks.
- * 
- *  Alexander Fuks, 10 May 2013.
+﻿/*
+ * Original source code is taken from http://www.codeproject.com/Articles/31823/RichTextBox-Cell-in-a-DataGridView
+ * Original class is published under CPOL license.
  */
 
 using System.Windows.Forms;
+using System;
+using System.ComponentModel;
+using System.Drawing;
 
 namespace RQS.Logic
 {
-    internal class DataGridViewRichTextBoxCell : DataGridViewCell
+    public class DataGridViewRichTextBoxCell : DataGridViewImageCell
     {
-        public DataGridViewRichTextBoxCell()
+        private static readonly RichTextBox _editingControl = new RichTextBox();
+
+        public override Type EditType
         {
-            
+            get
+            {
+                return typeof(DataGridViewRichTextBoxEditingControl);
+            }
         }
+
+        public override Type ValueType
+        {
+            get
+            {
+                return typeof(string);
+            }
+            set
+            {
+                base.ValueType = value;
+            }
+        }
+
+        public override Type FormattedValueType
+        {
+            get
+            {
+                return typeof(string);
+            }
+        }
+
+        private static void SetRichTextBoxText(RichTextBox ctl, string text)
+        {
+            try
+            {
+                ctl.Rtf = text;
+            }
+            catch (ArgumentException)
+            {
+                ctl.Text = text;
+            }
+        }
+
+        private Image GetRtfImage(int rowIndex, object value, bool selected)
+        {
+            Size cellSize = GetSize(rowIndex);
+
+            if (cellSize.Width < 1 || cellSize.Height < 1)
+                return null;
+
+            RichTextBox ctl = null;
+
+            if (ctl == null)
+            {
+                ctl = _editingControl;
+                ctl.Size = GetSize(rowIndex);
+                SetRichTextBoxText(ctl, Convert.ToString(value));
+            }
+
+            if (ctl != null)
+            {
+                // Print the content of RichTextBox to an image.
+                Size imgSize = new Size(cellSize.Width - 1, cellSize.Height - 1);
+                Image rtfImg = null;
+
+                if (selected)
+                {
+                    // Selected cell state
+                    ctl.BackColor = DataGridView.DefaultCellStyle.SelectionBackColor;
+                    ctl.ForeColor = DataGridView.DefaultCellStyle.SelectionForeColor;
+
+                    // Print image
+                    //rtfImg = RichTextBoxPrinter.Print(ctl, imgSize.Width, imgSize.Height);
+
+                    // Restore RichTextBox
+                    ctl.BackColor = DataGridView.DefaultCellStyle.BackColor;
+                    ctl.ForeColor = DataGridView.DefaultCellStyle.ForeColor;
+                }
+                else
+                {
+                    //rtfImg = RichTextBoxPrinter.Print(ctl, imgSize.Width, imgSize.Height);
+                }
+
+                return rtfImg;
+            }
+
+            return null;
+        }
+
+        public override void InitializeEditingControl(int rowIndex, object initialFormattedValue, DataGridViewCellStyle dataGridViewCellStyle)
+        {
+            base.InitializeEditingControl(rowIndex, initialFormattedValue, dataGridViewCellStyle);
+
+            RichTextBox ctl = DataGridView.EditingControl as RichTextBox;
+
+            if (ctl != null)
+            {
+                SetRichTextBoxText(ctl, Convert.ToString(initialFormattedValue));
+            }
+        }
+
+        protected override object GetFormattedValue(object value, int rowIndex, ref DataGridViewCellStyle cellStyle, TypeConverter valueTypeConverter, TypeConverter formattedValueTypeConverter, DataGridViewDataErrorContexts context)
+        {
+            return value;
+        }
+
+        protected override void Paint(Graphics graphics, Rectangle clipBounds, Rectangle cellBounds, int rowIndex, DataGridViewElementStates cellState, object value, object formattedValue, string errorText, DataGridViewCellStyle cellStyle, DataGridViewAdvancedBorderStyle advancedBorderStyle, DataGridViewPaintParts paintParts)
+        {
+            base.Paint(graphics, clipBounds, cellBounds, rowIndex, cellState, null, null, errorText, cellStyle, advancedBorderStyle, paintParts);
+
+            Image img = GetRtfImage(rowIndex, value, base.Selected);
+
+            if (img != null)
+                graphics.DrawImage(img, cellBounds.Left, cellBounds.Top);
+        }
+
+        #region Handlers of edit events, copyied from DataGridViewTextBoxCell
+
+        private byte flagsState;
+
+        protected override void OnEnter(int rowIndex, bool throughMouseClick)
+        {
+            base.OnEnter(rowIndex, throughMouseClick);
+
+            if ((base.DataGridView != null) && throughMouseClick)
+            {
+                this.flagsState = (byte)(this.flagsState | 1);
+            }
+        }
+
+        protected override void OnLeave(int rowIndex, bool throughMouseClick)
+        {
+            base.OnLeave(rowIndex, throughMouseClick);
+
+            if (base.DataGridView != null)
+            {
+                this.flagsState = (byte)(this.flagsState & -2);
+            }
+        }
+
+        protected override void OnMouseClick(DataGridViewCellMouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+            if (base.DataGridView != null)
+            {
+                Point currentCellAddress = base.DataGridView.CurrentCellAddress;
+
+                if (((currentCellAddress.X == e.ColumnIndex) && (currentCellAddress.Y == e.RowIndex)) && (e.Button == MouseButtons.Left))
+                {
+                    if ((this.flagsState & 1) != 0)
+                    {
+                        this.flagsState = (byte)(this.flagsState & -2);
+                    }
+                    else if (base.DataGridView.EditMode != DataGridViewEditMode.EditProgrammatically)
+                    {
+                        base.DataGridView.BeginEdit(false);
+                    }
+                }
+            }
+        }
+
+        public override bool KeyEntersEditMode(KeyEventArgs e)
+        {
+            return (((((char.IsLetterOrDigit((char)((ushort)e.KeyCode)) && ((e.KeyCode < Keys.F1) || (e.KeyCode > Keys.F24))) || ((e.KeyCode >= Keys.NumPad0) && (e.KeyCode <= Keys.Divide))) || (((e.KeyCode >= Keys.OemSemicolon) && (e.KeyCode <= Keys.OemBackslash)) || ((e.KeyCode == Keys.Space) && !e.Shift))) && (!e.Alt && !e.Control)) || base.KeyEntersEditMode(e));
+        }
+
+        #endregion
     }
 }
--- project/Logic/DataGridViewRichTextBoxColumn.cs
+++ project/Logic/DataGridViewRichTextBoxColumn.cs
@@ -1,28 +1,6 @@
-﻿/*   
- *  RQS
- *  Requirement Searching Utility
- *  Copyright (C) Fuks Alexander 2013
- *  
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *  
- *  Fuks Alexander, hereby disclaims all copyright
- *  interest in the program "RQS"
- *  (which makes passes at compilers)
- *  written by Alexander Fuks.
- * 
- *  Alexander Fuks, 10 May 2013.
+﻿/*
+ * Original source code is taken from http://www.codeproject.com/Articles/31823/RichTextBox-Cell-in-a-DataGridView
+ * Original class is published under CPOL license.
  */
 
 using System.Windows.Forms;
@@ -30,7 +8,7 @@ using System;
 
 namespace RQS.Logic
 {
-    internal class DataGridViewRichTextBoxColumn : DataGridViewColumn
+    public class DataGridViewRichTextBoxColumn : DataGridViewColumn
     {
         public DataGridViewRichTextBoxColumn()
             : base(new DataGridViewRichTextBoxCell())
@@ -46,12 +24,10 @@ namespace RQS.Logic
             set
             {
                 if (!(value is DataGridViewRichTextBoxCell))
-                    throw new InvalidCastException("CellTemplate must" +
-                          " be a DataGridViewRichTextBoxCell");
+                    throw new InvalidCastException("CellTemplate must be a DataGridViewRichTextBoxCell");
 
                 base.CellTemplate = value;
             }
         }
-
     }
 }
