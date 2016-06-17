20459dbf6677e752f1b6432d3599fb7de55e6299
Alexander Fuks <kungfux2010@gmail.com>
2016-06-18 01:21:03 +0300

    Tests are updated

--- Tests/headersTestCase.py
+++ Tests/headersTestCase.py
@@ -22,35 +22,60 @@ import socket
 fuseHostName = socket.gethostname()
 fuseIpAddress = socket.gethostbyname(fuseHostName)
 
-def test(method, uri):
+# Headers
+GET = 'GET'
+HEAD = 'HEAD'
+OPTIONS = 'OPTIONS'
+TRACE = 'TRACE'
+
+def doTest(method, uri):
 	conn = httplib.HTTPConnection(fuseIpAddress)
 	conn.request(method, uri)
 	return conn.getresponse().status
-
-def testHead(uri):
-	return test('HEAD', uri)
 	
-def testOptions():
-	return test('OPTIONS', '')
+class ResponceHeadersTestCase(unittest.TestCase):
+	# Incorrect request
+	def testIncorrectRequest(self):
+		self.assertEqual(doTest('', '/index.html'), 404)
 		
-class HeadersTestCase(unittest.TestCase):
-	def testEmptyRequest(self):
-		self.assertEqual(testHead(''), 200)
-	def testRootRequest(self):
-		self.assertEqual(testHead('/'), 200)
-	def testIndexFileRequest(self):
-		self.assertEqual(testHead('/index.html'), 200)
-	def testIndexWithParameterRequest(self):
-		self.assertEqual(testHead('/index.html?param=value'), 200)
-	def testIndexWithTwoParametersRequest(self):
-		self.assertEqual(testHead('/index.html?param1=&param2='), 200)
-	def testReadFolderUpRequest(self):
-		self.assertEqual(testHead('/../'), 400)
-	def testReadSystemDriveRequest(self):
-		self.assertEqual(testHead('C:\\'), 404)
-	def testReadFileFromSystemDriveRequest(self):
-		self.assertEqual(testHead('C:\\pagefile.sys'), 404)
-	def testReadFileFromWindowsDirectoryRequest(self):
-		self.assertEqual(testHead('%WINDIR%\\notepad.exe'), 404)
+	# GET request
+	def testGetEmptyRequest(self):
+		self.assertEqual(doTest(GET, ''), 200)
+	def testGetRootRequest(self):
+		self.assertEqual(doTest(GET, '/'), 200)
+	def testGetIndexFileRequest(self):
+		self.assertEqual(doTest(GET, '/index.html'), 200)
+	def testGetFolderUpLevelRequest(self):
+		self.assertEqual(doTest(GET, '/../'), 403)
+	def testGetSystemDriveRequest(self):
+		self.assertEqual(doTest(GET, 'C:\\'), 404)
+	def testGetCmdCommandRequest(self):
+		self.assertEqual(doTest(GET, 'echo "Hi"'), 404)
+	
+	# HEAD requests
+	def testHeadEmptyRequest(self):
+		self.assertEqual(doTest('HEAD', ''), 200)
+	def testHeadRootRequest(self):
+		self.assertEqual(doTest('HEAD', '/'), 200)
+	def testHeadIndexFileRequest(self):
+		self.assertEqual(doTest('HEAD', '/index.html'), 200)
+	def testHeadIndexWithParameterRequest(self):
+		self.assertEqual(doTest('HEAD', '/index.html?param=value'), 200)
+	def testHeadIndexWithTwoParametersRequest(self):
+		self.assertEqual(doTest('HEAD', '/index.html?param1=&param2='), 200)
+	def testHeadReadFolderUpRequest(self):
+		self.assertEqual(doTest('HEAD', '/../'), 403)
+	def testHeadReadSystemDriveRequest(self):
+		self.assertEqual(doTest('HEAD', 'C:\\'), 404)
+	def testHeadReadFileFromSystemDriveRequest(self):
+		self.assertEqual(doTest('HEAD', 'C:\\pagefile.sys'), 404)
+	def testHeadReadFileFromWindowsDirectoryRequest(self):
+		self.assertEqual(doTest('HEAD', '%WINDIR%\\notepad.exe'), 404)
+		
+	# OPTIONS request
 	def testOptionsMethod(self):
-		self.assertEqual(testOptions(), 200)
+		self.assertEqual(doTest(OPTIONS, ''), 200)
+		
+	# TRACE request (not suitable for now)
+	#def testTraceMethod(self):
+	#	self.assertEqual(doTest(TRACE, '/index.html'), 405)
--- Tests/telnetTestCase.py
+++ Tests/telnetTestCase.py
@@ -20,7 +20,7 @@ fusePort = 80
 # telnet config
 telnetTimeOut = 5
   
-def test(x):
+def doTest(x):
 	telnet = telnetlib.Telnet(fuseIpAddress, fusePort, telnetTimeOut)
 	telnet.write(x)
 	telnet.read_all()
@@ -29,14 +29,14 @@ def test(x):
 		
 class TelnetConnectionTestCase(unittest.TestCase):	
 	def testNoDataSentByClient(self):
-		self.assertTrue(test(''))
+		self.assertTrue(doTest(''))
 	def testNewLineSentByClient(self):
-		self.assertTrue(test('\n'))
+		self.assertTrue(doTest('\n'))
 	def testLsCommandSentByClient(self):
-		self.assertTrue(test('ln\n'))
+		self.assertTrue(doTest('ln\n'))
 	def testHttpOverTelnet(self):
-		self.assertTrue(test('GET /index.html HTTP/1.1'))
+		self.assertTrue(doTest('GET /index.html HTTP/1.1'))
 	def testHttpWithParameterOverTelnet(self):
-		self.assertTrue(test('GET /?by=fr&value=FR1-100 HTTP/1.1'))
+		self.assertTrue(doTest('GET /?by=fr&value=FR1-100 HTTP/1.1'))
 	def testHttpHeadOverTelnet(self):
-		self.assertTrue(test('HEAD /help.html HTTP/1.1'))
+		self.assertTrue(doTest('HEAD /help.html HTTP/1.1'))
