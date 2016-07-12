2984befbaf593c10099267d5b071d256bc2cc897
Alexander Fuks <kungfux2010@gmail.com>
2016-07-13 02:17:05 +0300

    Added info messages

--- ImportTool/Program.cs
+++ ImportTool/Program.cs
@@ -6,6 +6,10 @@ namespace ImportTool
 {
     internal static class Program
     {
+        private const string MessageFilesCanBeSkipped = "Note: Not qualified file(s) by extension will be skipped from import.";
+        private const string MessageFilesImported = "file(s) have been processed.";
+
+        private static int _filesProcessed;
         private static string _lastProcessedFile;
 
         private static void Main(string[] args)
@@ -14,11 +18,14 @@ namespace ImportTool
 
             var options = new Options();
             CommandLine.Parser.Default.ParseArguments(args, options);
+
             if (string.IsNullOrEmpty(options.File) && string.IsNullOrEmpty(options.Directory))
             {
                 Console.WriteLine(options.GetUsage());
                 Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
             }
+
+            Console.WriteLine(MessageFilesCanBeSkipped);
 
             var parser = new RequirementsFileParser();
             parser.OnUpdateStatus += Parser_OnUpdateStatus;
@@ -34,16 +41,23 @@ namespace ImportTool
             }
 
             Console.WriteLine();
+            Console.WriteLine($"{_filesProcessed} {MessageFilesImported}");
+
         }
 
         private static void Parser_OnUpdateStatus(ProgressEventArgs e)
         {
+            if (_lastProcessedFile != e.FileBeingProcessed)
+                _filesProcessed++;
+
             if (!string.IsNullOrEmpty(_lastProcessedFile))
             {
                 Console.Write(_lastProcessedFile != e.FileBeingProcessed ? "\r\n" : "\r");
             }
+
             _lastProcessedFile = e.FileBeingProcessed;
-            Console.Write($"File: {e.FileBeingProcessed} Record: {e.RecordNumberBeingProcessed} Completed: {e.PercentsComplete}%");
+
+            Console.Write($"Processing file: {e.FileBeingProcessed} - {e.RecordNumberBeingProcessed} record(s) [{e.PercentsComplete}%]");
         }
     }
 }
