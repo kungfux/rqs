683289fb1e62ec9cd40d28cf2b410888b673ae61
Alexander Fuks <kungfux2010@gmail.com>
2016-07-18 23:40:25 +0300

    Added parameter to call the auto-refreshing

--- www/js/fuse-debug.js
+++ www/js/fuse-debug.js
@@ -1,5 +1,7 @@
 $(document).ready(function (){
-	startRefresh();
+	if (getUrlParameter('refresh')) {
+		startRefresh();
+	}
 });
 
 function startRefresh(){
@@ -9,3 +11,18 @@ function startRefresh(){
         window.location = location.href;
     }
 }
+
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
