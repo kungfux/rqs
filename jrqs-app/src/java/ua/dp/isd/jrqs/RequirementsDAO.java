6e902a9ddcace12f1cb352e87c3dd42650920e30
Alexander Fuks <Alexander.V.Fuks@gmail.com>
2017-06-21 21:47:09 +0300

    Apply review comments

--- jrqs-app/src/java/ua/dp/isd/jrqs/RequirementsDAO.java
+++ jrqs-app/src/java/ua/dp/isd/jrqs/RequirementsDAO.java
@@ -26,10 +26,16 @@
  */
 package ua.dp.isd.jrqs;
 
-import java.sql.*;
-import java.util.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
-import javax.naming.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class RequirementsDAO {
